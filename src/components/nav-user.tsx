27cf8fc610f5bfbc2d77ca7ca85295298e4f8de7
FernandoDzay <luisf.dzay@gmail.com>
2025-08-27 18:35:24 -0600

    Primera verssion para prod

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,33 +1,7 @@
-import {
-    IconCreditCard,
-    IconDotsVertical,
-    IconLogout,
-    IconNotification,
-    IconUserCircle,
-} from '@tabler/icons-react';
-
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import {
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-    useSidebar,
-} from '@/components/ui/sidebar';
+import { SidebarMenu, SidebarMenuItem } from '@/components/ui/sidebar';
 import { UserButton } from '@daveyplate/better-auth-ui';
 
 export function NavUser() {
-    const { isMobile } = useSidebar();
-    // session
-
     return (
         <SidebarMenu>
             <SidebarMenuItem>
